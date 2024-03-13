0294e2d741674209dba556b268d9546f4c9ce2ab
mstsurnalyakter <surnalyms@gmail.com>
2024-03-13 10:22:20 +0600

    show dynamic data in web page

---
+++ src/components/Cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <div className='bg-base-100 shadow-xl min-h-4'>
+        <h2>Cart</h2>
+    </div>
+  )
+}
+
+export default Cart
---
+++ src/components/Courses/Courses.jsx
@@ -0,0 +1,37 @@
+// import PropTypes from 'prop-types'
+import Course from './Course'
+import { useEffect, useState } from "react"
+
+const Courses = () => {
+    const [courses,setCourses] = useState([]);
+
+    useEffect(() => {
+      const loadCourses = async () => {
+        try {
+          const res = await fetch("courses.json");
+          if (!res.ok) {
+            throw new Error("Failed to fetch data");
+          }
+          const data = await res.json();
+          setCourses(data);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+      loadCourses();
+    }, []);
+
+  return (
+    <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 lg:col-span-3'>
+
+        {
+            courses.map(course=><Course course={course} key={course.id} />)
+        }
+    </div>
+  )
+}
+
+// Courses.propTypes = {}
+
+export default Courses
